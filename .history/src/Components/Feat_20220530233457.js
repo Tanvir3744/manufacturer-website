174398165154dc0663e2eb64257d5933e8db78f4
Tanvir3744 <biplobmohammad1@gmail.com>
2022-05-30 23:36:03 +0600

    final commit

---
+++ .history/src/Components/Feat_20220530233457.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Feat = () => {
+    return (
+        <div>
+            <div class="hero min-h-screen bg-base-200">
+                <div class="hero-content flex-col lg:flex-row">
+                    <img src="https://chromium.themes.zone/local/wp-content/uploads/sites/4/2017/06/prod1_deposit_opt-300x300.jpg" class="max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 class="text-5xl font-bold">Our Feature product</h1>
+                        <p class="py-6">most qualityfull and expensive products that we have...</p>
+                        <button class="btn btn-primary">Learn More</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Feat;
